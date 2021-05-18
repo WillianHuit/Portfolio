4c3f6ec115ea90336a0037e8270fb4a728718044
WillianHuit <winhuit98@hotmail.com>
2021-05-17 18:08:46 -0600

    remove call back
    Try to fix bug

--- src/js/designsScript.js
+++ src/js/designsScript.js
@@ -22,30 +22,3 @@ let options={
 	rootMargin: '0px',
 	threshold: 0.25
 };
-let callback = (entries, observer)=>{
-	entries.forEach(entry=>{
-		if(entry.isIntersecting && entry.target.className ==='image-gallery'){
-			let imageUrl = entry.target.getAttribute('data-img');
-			if(imageUrl){
-				entry.target.src = imageUrl;
-				observer.unobserve(entry.target);
-			}
-		}
-	});
-};
-let observer = new IntersectionObserver(callback, options);
-observer.observe(document.querySelector('#img1'));
-observer.observe(document.querySelector('#img2'));
-observer.observe(document.querySelector('#img3'));
-observer.observe(document.querySelector('#img4'));
-observer.observe(document.querySelector('#img5'));
-observer.observe(document.querySelector('#img6'));
-observer.observe(document.querySelector('#img7'));
-observer.observe(document.querySelector('#img8'));
-observer.observe(document.querySelector('#img9'));
-observer.observe(document.querySelector('#img10'));
-observer.observe(document.querySelector('#img11'));
-observer.observe(document.querySelector('#img12'));
-observer.observe(document.querySelector('#img13'));
-observer.observe(document.querySelector('#img14'));
-observer.observe(document.querySelector('#img15'));
--- src/js/projects.js
+++ src/js/projects.js
@@ -1,7 +1,6 @@
 
 
 function viewMore(e){
-	console.log(e.childNodes);
 	if(e.childNodes[1].className  == "info-project-hide"){
 		e.childNodes[1].className  = "info-project-show";
 	}else{
