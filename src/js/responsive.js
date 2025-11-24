64889137f598fe5d9f503bfbf74c7acf27dd5dd5
willianhuit98 <winhuit98@hotmail.com>
2025-11-24 16:41:50 -0600

    limpieza de archivos

--- src/js/responsive.js
+++
@@ -1,8 +0,0 @@
-function btnMenuClick(){
-	var x = document.getElementById("itemsBar");
-  if (x.className == "items-bar") {
-    x.className = "responsive";
-  } else {
-    x.className = "items-bar";
-  }
-}
